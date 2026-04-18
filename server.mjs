bb78aad231e12528d92b2843631f3b91847f1f71
BypassHub-EX <bloomhavengg@gmail.com>
2026-04-18 17:15:40 +0300

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 app.disable("x-powered-by");
 
-const LUA_PAYLOAD = `print("soon"))
+const LUA_PAYLOAD = `print("soon")`
 `.trim();
 
 /* MAIN LOADER */
